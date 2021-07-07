f0e525a3c3f7ca08f9ca9ba89901a95270a381b5
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-07-07 16:02:50 +0530

    using switch case

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,30 +1,27 @@
 import java.util.Random;
-
 public class EmpWageComputation {
 
         public static void main(String args[]) {
 
                 System.out.println("Welcome to Employee Wage Computation Program");
-                Random num = new Random ();
+		 Random num = new Random ();
                 int CheckAvail= num.nextInt(3);
                 System.out.println(" CheckAvail is " + CheckAvail);
-                int WagePerHour = 20;
-                int FulldayHour = 8;
-                int PartTimeHour = 8;
-          if (CheckAvail == 1)
-          {
-			int EmpFulldayWage =  WagePerHour * FulldayHour ;
-			System.out.println("Employee is Present and Employee fullday Wage is " + EmpFulldayWage);
-          }
-          else if (CheckAvail == 2)
-                {
-                	int EmpHalfdayWage =  WagePerHour * PartTimeHour ;
-                	System.out.println("Employee is Present and Employee half day Wage is " + EmpHalfdayWage);
-                }
-          		else 
-          			System.out.println("Employee is Absent " );
-
+		int WagePerHour = 20;
+		int FulldayHour = 8;
+		int PartTimeHour = 8;
+                switch  (CheckAvail) {
+		        	case 0:
+		        		System.out.println("Employee is Absent " );
+		        		break;
+		        	case 1:
+                         int EmpFulldayWage =  WagePerHour * FulldayHour ;
+                         System.out.println("Employee is Present and Employee fullday Wage is " + EmpFulldayWage);
+                         break;
+		        	case 2:
+		        		int EmpHalfdayWage =  WagePerHour * PartTimeHour ;
+		                System.out.println("Employee is Present and Employee half day Wage is " + EmpHalfdayWage);
+		                break;
+				}
         }
-
-
 }
