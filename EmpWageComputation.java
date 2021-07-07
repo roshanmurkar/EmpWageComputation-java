9f6f757613660077b715671ff584c42c8528f676
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-07-07 15:48:35 +0530

    Emp Daily wage calculated

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,3 +1,4 @@
+  
 import java.util.Random;
 
 public class EmpWageComputation {
@@ -5,16 +6,18 @@ public class EmpWageComputation {
         public static void main(String args[]) {
 
                 System.out.println("Welcome to Employee Wage Computation Program");
-		 Random num = new Random ();
+                Random num = new Random ();
                 int CheckAvail= num.nextInt(2);
                 System.out.println(" CheckAvail is " + CheckAvail);
-
+                int WagePerHour = 20;
+                int FulldayHour = 8;
                 if (CheckAvail == 1)
-                		System.out.println("Employee is Present");
+                {
+                	int DailyEmpWage =  WagePerHour *FulldayHour ;
+                	System.out.println("Employee is Present and Employee Daily Wage is " + DailyEmpWage);
+                }
                 else 
-                		System.out.println("Employee is Absent");
-
-                
+                    System.out.println("Employee is Absent");
 
 	}
 }
