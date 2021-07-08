04d444e6ae4936ceff91f1b8ac747422bfddcfdf
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-07-08 09:51:20 +0530

    month Wage calculation

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,25 +3,47 @@ public class EmpWageComputation {
 
         public static void main(String args[]) {
 
-                System.out.println("Welcome to Employee Wage Computation Program");
-		 Random num = new Random ();
-                int CheckAvail= num.nextInt(3);
-                System.out.println(" CheckAvail is " + CheckAvail);
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+		Random num = new Random ();
 		int WagePerHour = 20;
 		int FulldayHour = 8;
-		int PartTimeHour = 8;
-                switch  (CheckAvail) {
-		        	case 0:
-		        		System.out.println("Employee is Absent " );
-		        		break;
-		        	case 1:
-                         int EmpFulldayWage =  WagePerHour * FulldayHour ;
-                         System.out.println("Employee is Present and Employee fullday Wage is " + EmpFulldayWage);
-                         break;
-		        	case 2:
-		        		int EmpHalfdayWage =  WagePerHour * PartTimeHour ;
-		                System.out.println("Employee is Present and Employee half day Wage is " + EmpHalfdayWage);
-		                break;
+		int PartTimeHour= 8;
+		int NumWorkingDays=20;
+		int Totalsalary=0;
+		int salary1=0;
+		int salary2=0;
+		int salary3=0;
+		int sal2=0;
+		int sal3=0;
+		int Absent=0;
+		int Fullday=0;
+		int Halfday=0;
+		
+		for (int day=1; day<= NumWorkingDays; day++){
+				int CheckAvail= num.nextInt(3);
+    
+				switch  (CheckAvail) {
+	            case 0:
+		 			Absent++;
+					salary1= 0 * WagePerHour;
+					break;
+				case 1:
+					Fullday++;
+					sal2=  FulldayHour * WagePerHour;
+					salary2= salary2 + sal2 ;
+					break;
+				case 2:
+					Halfday++;
+					sal3= PartTimeHour * WagePerHour;
+					salary3 = salary3 + sal3 ;
+					break;
 				}
-        }
+		}
+				 Totalsalary = salary1 + salary2 + salary3 ;
+					System.out.println( "Number of days Absent is " + Absent );
+					System.out.println( "Number of Fulldays present is  " + Fullday );
+					System.out.println( "Number of Halfdays present is " +Halfday );
+                    System.out.println( "The total salary of the employee is  " + Totalsalary );
+	}
 }
